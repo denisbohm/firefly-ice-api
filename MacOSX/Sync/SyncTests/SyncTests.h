b8ca40f49d2372590d166488603b35a02469e98c
Denis Bohm <denis@fireflydesign.com>
2014-03-13 10:10:30 -0500

    update project settings

--- MacOSX/Sync/SyncTests/SyncTests.h
+++ MacOSX/Sync/SyncTests/SyncTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 Firefly Design. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface SyncTests : SenTestCase
+@interface SyncTests : XCTestCase
 
 @end
