e39f6a741a219e94f4087fa454643958c3cea78e
Denis Bohm <denis@fireflydesign.com>
2014-06-23 12:55:55 -0500

    update release number

--- FireflyDevice/FireflyDevice/FDBundle.m
+++ FireflyDevice/FireflyDevice/FDBundle.m
@@ -20,8 +20,8 @@
 {
     return @{
         @"CFBundleName": @"FireflyDevice",
-        @"CFBundleShortVersionString": @"1.0.16",
-        @"CFBundleVersion": @"16",
+        @"CFBundleShortVersionString": @"1.0.17",
+        @"CFBundleVersion": @"17",
         @"NSHumanReadableCopyright": @"Copyright © 2013-2014 Firefly Design LLC / Denis Bohm. All rights reserved.",
     };
 }
