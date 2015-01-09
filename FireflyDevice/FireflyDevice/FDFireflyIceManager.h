c7d6ac28d7dbc705e3e1af4653575db552801e78
Denis Bohm <denis@fireflydesign.com>
2015-01-08 19:57:24 -0600

    add advertisement callback

--- FireflyDevice/FireflyDevice/FDFireflyIceManager.h
+++ FireflyDevice/FireflyDevice/FDFireflyIceManager.h
@@ -26,6 +26,7 @@
 @optional
 
 - (void)fireflyIceManager:(FDFireflyIceManager *)manager advertisementDataHasChanged:(FDFireflyIce *)fireflyIce;
+- (void)fireflyIceManager:(FDFireflyIceManager *)manager advertisement:(FDFireflyIce *)fireflyIce;
 
 - (void)fireflyIceManager:(FDFireflyIceManager *)manager openedBLE:(FDFireflyIce *)fireflyIce;
 - (void)fireflyIceManager:(FDFireflyIceManager *)manager closedBLE:(FDFireflyIce *)fireflyIce;
