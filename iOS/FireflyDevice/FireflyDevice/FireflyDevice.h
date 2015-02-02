35ebc15cac249a84999fa65686c8f95cf79d9f38
Denis Bohm <denis@fireflydesign.com>
2015-02-01 18:59:38 -0600

    fixup module imports; add separate directories for iOS and OS X frameworks

---
+++ iOS/FireflyDevice/FireflyDevice/FireflyDevice.h
@@ -0,0 +1,46 @@
+//
+//  FireflyDevice.h
+//  FireflyDevice
+//
+//  Created by Denis Bohm on 2/1/15.
+//  Copyright (c) 2015 Firefly Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FireflyDevice.
+FOUNDATION_EXPORT double FireflyDeviceVersionNumber;
+
+//! Project version string for FireflyDevice.
+FOUNDATION_EXPORT const unsigned char FireflyDeviceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FireflyDevice/PublicHeader.h>
+
+
+#import <FireflyDevice/FDBinary.h>
+#import <FireflyDevice/FDBundle.h>
+#import <FireflyDevice/FDBundleManager.h>
+#import <FireflyDevice/FDCrypto.h>
+#import <FireflyDevice/FDDetour.h>
+#import <FireflyDevice/FDDetourSource.h>
+#import <FireflyDevice/FDExecutor.h>
+#import <FireflyDevice/FDFileLog.h>
+#import <FireflyDevice/FDFireflyDeviceLogger.h>
+#import <FireflyDevice/FDFireflyIce.h>
+#import <FireflyDevice/FDFireflyIceChannel.h>
+#import <FireflyDevice/FDFireflyIceChannelBLE.h>
+#import <FireflyDevice/FDFireflyIceChannelMock.h>
+#import <FireflyDevice/FDFireflyIceCoder.h>
+#import <FireflyDevice/FDFireflyIceDeviceMock.h>
+#import <FireflyDevice/FDFireflyIceManager.h>
+#import <FireflyDevice/FDFireflyIceSimpleTask.h>
+#import <FireflyDevice/FDFireflyIceTaskSteps.h>
+#import <FireflyDevice/FDFirmwareUpdateTask.h>
+#import <FireflyDevice/FDHardwareId.h>
+#import <FireflyDevice/FDHelloTask.h>
+#import <FireflyDevice/FDIEEE754.h>
+#import <FireflyDevice/FDIntelHex.h>
+#import <FireflyDevice/FDJSON.h>
+#import <FireflyDevice/FDObservable.h>
+#import <FireflyDevice/FDSyncTask.h>
+#import <FireflyDevice/FDWeak.h>
