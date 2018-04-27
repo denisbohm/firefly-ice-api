de7a2ecd539ffa380fb476d997e7dd0ed3687950
Denis Bohm <denis@fireflydesign.com>
2018-04-27 15:09:29 -0500

    update universal script to handle dependencies

--- iOS/FireflyDevice/universal.sh
+++ iOS/FireflyDevice/universal.sh
@@ -8,33 +8,42 @@ else
     name=$1
 fi
 
+configuration=Debug
 framework=`echo ${name} | tr ' ' '_'`
-log=build/build.log
+log=derived/build.log
 
 echo "building iOS framework ${name}..."
 
-mkdir -p build
+rm -rf derived
+mkdir -p derived
 
-xcodebuild -configuration Release clean build -scheme "${name}" -sdk iphoneos SYMROOT=build >>"${log}" 2>&1
+xcodebuild -configuration "${configuration}" clean build -scheme "${name}" -sdk iphoneos -derivedDataPath derived >>"${log}" 2>&1
 exit_code=$?
 if [ $exit_code != 0 ]
 then
     echo "FAILED: build iphoneos framework ${name}"
 fi
 
-xcodebuild -configuration Release clean build -scheme "${name}" -sdk iphonesimulator SYMROOT=build >>"${log}" 2>&1
+xcodebuild -configuration "${configuration}" clean build -scheme "${name}" -sdk iphonesimulator -derivedDataPath derived >>"${log}" 2>&1
 exit_code=$?
 if [ $exit_code != 0 ]
 then
     echo "FAILED: build iphonesimulator framework ${name}"
 fi
 
-cp -RL "build/Release-iphoneos" "build/Release-universal"
-if [ -f "build/Release-iphonesimulator/${framework}.framework/Modules/${framework}.swiftmodule" ]; then
-    echo "exists"
-    cp -RL "build/Release-iphonesimulator/${framework}.framework/Modules/${framework}.swiftmodule"/* \
-           "build/Release-universal/${framework}.framework/Modules/${framework}.swiftmodule/"
+cp -RL "derived/Build/Products/${configuration}-iphoneos" "derived/Build/Products/${configuration}-universal"
+xcodebuild -project "${name}.xcodeproj" -list | sed -n '/Schemes/,/^$/p' | grep -v "Schemes:" > derived/schemes.txt
+schemes=`sed '$ d' derived/schemes.txt`
+while read name; do
+echo "generating universal for ${name}"
+framework=`echo ${name} | tr ' ' '_'`
+
+if [ -d "derived/Build/Products/${configuration}-iphonesimulator/${framework}.framework/Modules/${framework}.swiftmodule" ]; then
+    cp -RL "derived/Build/Products/${configuration}-iphonesimulator/${framework}.framework/Modules/${framework}.swiftmodule"/* \
+           "derived/Build/Products/${configuration}-universal/${framework}.framework/Modules/${framework}.swiftmodule/"
 fi
-lipo -create "build/Release-iphoneos/${framework}.framework/${framework}" \
-             "build/Release-iphonesimulator/${framework}.framework/${framework}" \
-     -output "build/Release-universal/${framework}.framework/${framework}"
+lipo -create "derived/Build/Products/${configuration}-iphoneos/${framework}.framework/${framework}" \
+             "derived/Build/Products/${configuration}-iphonesimulator/${framework}.framework/${framework}" \
+     -output "derived/Build/Products/${configuration}-universal/${framework}.framework/${framework}"
+
+done <<< "${schemes}"
